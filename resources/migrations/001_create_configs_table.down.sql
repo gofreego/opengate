3991f0fc179b1762c36071b985b2dc14c1623ae9
Pavan Yewale <pavanyewale1996@gmail.com>
2026-06-23 23:16:07 +0530

    migrations corrected

--- resources/migrations/001_create_configs_table.down.sql
+++ resources/migrations/001_create_configs_table.down.sql
@@ -5,12 +5,12 @@
 -- Drop trigger first
 DROP TRIGGER IF EXISTS update_configs_updated_at ON configs;
 
--- Drop function
-DROP FUNCTION IF EXISTS update_updated_at_column();
-
 -- Drop indexes (will be dropped with table, but explicit for clarity)
 DROP INDEX IF EXISTS idx_configs_path_prefix;
 DROP INDEX IF EXISTS idx_configs_name;
 
 -- Drop table
 DROP TABLE IF EXISTS configs;
+
+-- Drop function
+DROP FUNCTION IF EXISTS update_updated_at_column();
